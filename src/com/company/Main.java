a2f289235b5c4beb05e35204cb17c28c0c74c26d
Vasilisa Nichipor <vsilfox@gmail.com>
2022-05-24 19:48:21 +0300

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,58 @@
+package com.company;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John");
+        List<String> families = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson", "Brown");
+        Collection<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 10_000_000; i++) {
+            persons.add(new Person(
+                    names.get(new Random().nextInt(names.size())),
+                    families.get(new Random().nextInt(families.size())),
+                    new Random().nextInt(100),
+                    Sex.values()[new Random().nextInt(Sex.values().length)],
+                    Education.values()[new Random().nextInt(Education.values().length)])
+            );
+        }
+
+        long numberOfChildren = persons.stream()
+                .filter(x -> x.getAge() < 18)
+                .count();
+
+        System.out.println(numberOfChildren);
+
+        List<String> conscripts = persons.stream()
+                .filter(x -> x.getSex() == Sex.MAN)
+                .filter(x -> x.getAge() >= 18 && x.getAge() < 27)
+                .map(Person::getFamily)
+                .collect(Collectors.toList());
+
+        int count = 0;
+        for (String conscript : conscripts) {
+            System.out.println(conscript);
+            count++;
+            if (count > 10) break;
+        }
+
+        List<Person> workablePersons = persons.stream()
+                .filter(x -> x.getEducation() == Education.HIGHER)
+                .filter(x -> x.getAge() >= 18)
+                .filter(x -> (x.getSex() == Sex.WOMAN && x.getAge() < 60) ||
+                        (x.getSex() == Sex.MAN && x.getAge() < 65))
+                .sorted(Comparator.comparing(Person::getFamily))
+                .collect(Collectors.toList());
+
+        count = 0;
+        for (Person workablePerson : workablePersons) {
+            System.out.println(workablePerson.toString());
+            count++;
+            if (count > 10) {
+                break;
+            }
+        }
+    }
+}
